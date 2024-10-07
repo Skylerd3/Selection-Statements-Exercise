7591a56b7a73dc815a981b71b5bd96afd8352ba0
skyler dennis <skylerdenniss11@gmail.com>
2024-10-07 15:35:14 -0400

    Finnished creating the numbers guessing game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var r = new Random();
+
+            int favNumber = 5;
+
+            Console.WriteLine("Try and guess the number from 1 to 1000");
+            int userInput = Console.ReadLine();
+
+            if(userInput > favNumber)
+            {
+                Console.Writeline("nope, Too high!");
+
+            }
+            if else(userInput < favNumber)
+            {
+                Console.WriteLine("Almost, But to low!");
+            }
+            else(userInput == favNumber)
+            {
+                Console.WriteLine("CORRECT!!!");
+            }
+
+
+
+            
         }
     }
 }
